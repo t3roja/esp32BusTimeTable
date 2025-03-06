b677e9bf1773c394f8d3643d032fd16f9229a6ec
t3roja <ronkainen@hotmail.fi>
2025-03-06 14:30:10 +0200

    First screen mockup

--- ScreenTest/ScreenTest.ino
+++ ScreenTest/ScreenTest.ino
@@ -1,5 +1,4 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7789.h>
@@ -12,37 +11,69 @@
 #define TFT_MOSI 23  // MOSI
 #define TFT_SCLK 18  // SCLK
 
-// Wifi SSID and password
-const char WIFI_SSD[] = "SSID";
-const char WIFI_PASSWORD[] = "PASSWORD";
-
-// hostname, pathname and query
-const char SERVER_URL = "192.168.1:5000/data";
-
+// TFT Screen
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
-int rotation = 0;
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Initializing ST7789...");
+//  To draw a line: tft.drawLine(51, 0, 51, 80, ST77XX_BLACK); // Starting point X, y, ending point x, y, color
 
-  tft.init(170, 320);
-  Serial.println("ST7789 Initialized!");
-
-  tft.setRotation(0);  // Try 0, 1, 2, 3
-  Serial.println("Rotation set!");
-
-  tft.fillScreen(ST77XX_WHITE);
-  delay(100);  // Allow screen refresh
-
-  // Draw test rectangle
-  tft.fillRect(30, 100, 130, 40, ST77XX_BLUE); //(X-coordinat of top-left of the rectangle, Y-coordinate of top-left, width(pixels), height(pixels), color)
-  tft.setCursor(40, 110);
-  tft.setTextColor(ST77XX_WHITE);  // White text on blue
-  tft.setTextSize(2);
-  tft.println("Screen test");
-  Serial.println("Text printed!");
+void textToScreen(String text) {
+  tft.fillScreen(ST77XX_BLUE);
+  tft.setCursor(10, 110);
+  tft.println(text);
 }
 
+void setup() {
+
+  // Initialization
+  Serial.begin(115200);
+  tft.init(170, 320);
+  tft.fillScreen(ST77XX_MAGENTA);
+  tft.setRotation(1);  // 0, 1, 2, 3
+
+  tft.drawRect(0, 0, 320, 170, ST77XX_WHITE); //(X-coordinate of top-left of the rectangle, Y-coordinate of top-left, width(pixels), height(pixels), color)
+  tft.drawRect(0, 40, 80, 60, ST77XX_WHITE);
+  tft.drawRect(80,40,240, 60, ST77XX_WHITE);
+
+  tft.setTextSize(2);
+  tft.setCursor(10,10);
+  tft.print("-12'C A");
+  tft.setCursor(220, 10);
+  tft.print("06:27");
+
+  tft.setTextSize(6);
+  tft.setCursor(5, 50);
+  tft.setTextColor(ST77XX_WHITE);
+  
+  tft.println("10");
+
+  tft.setTextSize(3);
+  tft.setCursor(90, 60);
+  tft.print("SAAPUU ");
+
+  tft.print("06:32");
+
+  tft.setCursor(10, 110);
+  tft.setTextSize(2);
+  tft.print("Seuraavat ");
+
+
+  tft.setCursor(160, 110);
+  tft.print("10");
+  tft.setCursor(220, 110);
+  tft.print("06:47");
+
+
+  tft.setCursor(160, 140);
+  tft.print("9");
+  tft.setCursor(220, 140);
+  tft.print("07:02");
+
+  // Draw test rectangle
+  //tft.fillRect(30, 100, 130, 40, ST77XX_BLUE); //(X-coordinate of top-left of the rectangle, Y-coordinate of top-left, width(pixels), height(pixels), color)
+
+}
+
+
 void loop() {
+
 }
